e60dc9e42aef32844c7acc2b2230b060709bc90d
Chris O'Neil <chris.oneil@gmail.com>
2017-06-25 01:33:09 +0100

    Install the Windows based version of Vim
    I've tested, and it seems the Git Bash and Windows versions of this can
    sit alongside each other.

--- initial-bootstrap.bat
+++ initial-bootstrap.bat
@@ -9,7 +9,7 @@ set source_code_pro_font_url="https://github.com/ryanoasis/nerd-fonts/raw/master
 if not exist %choco_install_path% (
     @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 )
-choco install -y 7zip conemu curl git keepass 
+choco install -y 7zip conemu curl git keepass vim
 if not exist %terminal_connector_path% (
     curl -k -L --output connector.7z https://github.com/Maximus5/cygwin-connector/releases/download/v0.7.4/terminals.v0.7.4.7z
     7z e connector.7z -o%git_install_path% conemu-msys2-64.exe -r
